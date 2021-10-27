c0e86a5594544456ceb9bcc7658392f68c43b026
Andrew Makhnev <andr.xray@mail.ru>
2021-10-27 18:36:55 +0300

    added 3 intefaces: IFields for fields, IFileIO for reading from file, IParser for parsing data in files

---
+++ IFields.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lw1
+{
+    interface IFields
+    {
+        string FIPS { get; set; }
+        string Admin2 { get; set; }
+        string Province_State { get; set; }
+        string Country_Region { get; set; }
+        string Last_Update { get; set; }
+        string Lat { get; set; }
+        string Long_ { get; set; }
+        string Confirmed { get; set; }
+        string Deaths { get; set; }
+        string Recovered { get; set; }
+        string Active { get; set; }
+        string Combined_Key { get; set; }
+        string Incident_Rate { get; set; }
+        string Case_Fatality_Ratio { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,40 +6,9 @@ namespace lw1
 {
     class Program
     {
-        static string mainPath = @"..\..\..\..\..\COVID-19-master\COVID-19-master\csse_covid_19_data\csse_covid_19_daily_reports";
-        //static string mainPathUs = @"..\..\..\..\..\COVID-19-master\COVID-19-master\csse_covid_19_data\csse_covid_19_daily_reports_us";
-        static string[] fileArr = Directory.GetFiles($@"{mainPath}", "*.csv");
-
-        public static void GetParsedData(string path, TextFieldParser parsedFile)
-        {
-            string dataLine;
-            var columns = Parser.GetColumnsNames(path);
-
-            while ((dataLine = parsedFile.ReadLine()) != null)
-            {
-                var allData = Parser.GetAllData(path, dataLine);
-
-                for (int i = 0; i < columns.Length; i++)
-                {
-                    foreach (var data in allData)
-                    {
-                        Console.WriteLine($"{columns[i]}: {data}");
-                        i++;
-                    }
-                }
-            }
-        }
-
         static void Main(string[] args)
         {
-            foreach (var file in fileArr)
-            {
-                var actualPath = file.ToString();
-                var parsedFile = Parser.SetParserParameters(actualPath);
-                parsedFile.ReadLine();
-
-                GetParsedData(actualPath, parsedFile);
-            }
+            
         }
     }
 }
