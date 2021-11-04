52efcf11527e2872a592d2ef5d6d769019034859
Andrew Makhnev <andr.xray@mail.ru>
2021-11-04 14:32:18 +0300

    added class SqlConn and interface ISqlConn which allow set connection to sql db

--- Program.cs
+++ Program.cs
@@ -8,32 +8,37 @@ namespace lw1
     {
         static void Main(string[] args)
         {
+            ISqlConn db = new SqlConn();
+            db.openConnection();
+            db.getConnection();
+            db.closeConnection();
+
             //проверка на корректное кол-во аргументов программы
             IArgumentParser argsValidation = new ArgumentParser();
             argsValidation.CheckNumberOfArgs(args);
 
-            for(int i = 0; i < args.Length; i++) //цикл для обхода всех директорий с файлами
-            {
-                string[] fileArr = Directory.GetFiles($@"{args[i]}", "*.csv"); //инициализация массива файлов в определенной дериктории
+            //for(int i = 0; i < args.Length; i++) //цикл для обхода всех директорий с файлами
+            //{
+            //    string[] fileArr = Directory.GetFiles($@"{args[i]}", "*.csv"); //инициализация массива файлов в определенной дериктории
 
-                IFileIO file = new FileIO();
+            //    IFileIO file = new FileIO();
 
-                var dataArr = file.ReadFile(fileArr[0]);
+            //    var dataArr = file.ReadFile(fileArr[0]);
 
-                int height = dataArr.GetLength(0);
-                int width = dataArr.GetLength(1);
+            //    int height = dataArr.GetLength(0);
+            //    int width = dataArr.GetLength(1);
 
 
-                //проверка корректности данных в двумерном массиве 
-                for(int y = 0; y < height; y++)
-                {
-                    for(int x = 0; x < width; x++)
-                    {
-                        Console.Write(dataArr[y, x] + "|");
-                    }
-                    Console.WriteLine();
-                }
-            }
+            //    //проверка корректности данных в двумерном массиве 
+            //    for(int y = 0; y < height; y++)
+            //    {
+            //        for(int x = 0; x < width; x++)
+            //        {
+            //            Console.Write(dataArr[y, x] + "|");
+            //        }
+            //        Console.WriteLine();
+            //    }
+            //}
         }
     }
 }
